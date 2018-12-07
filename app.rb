d087a8cbd11281008d5fb61234c9aef5f1186370
Daniel Burbach <daniel@>
2018-12-07 11:17:25 -0800

    added html to other erb pages working on getting dynamic routin list to show

--- app.rb
+++ app.rb
@@ -11,23 +11,36 @@ get ('/') do
   erb(:projects_home)
 end
 
-post('/add_project') do
-  project = Project.new(params)
-  project.save
-  @title = project.title
+get('/details/:id') do
+  @projects = Project.find(params[:id])
+  @volunteer = Project.find(params[:id])
+  erb(:project_info)
+end
+
+get('/add_project') do
+  @title = Project.find(params[:id])
   @projects = Project.all
   erb(:projects_home)
 end
 
-post('/add_volunteer') do
-  volunteer = Volunteer.new(params)
-  volunteer.save
-  @name = volunteer.name
-  @volunteer = Volunteer.all
+post('/add_project') do
+  project = Project.new(params)
+  @title = project.title
+  @projects = Project.all
+  project.save
   erb(:projects_home)
 end
 
-get('/details/:id') do
-  @project = Project.find(params[:id])
-  erb(:project_info)
+get('/add_volunteer') do
+  @name = Volunteer.find(params[:id])
+  @volunteer = Volunteer.all
+  erb(:volunteer_info)
+end
+
+post('/add_volunteer') do
+  volunteer = Volunteer.new(params)
+  @name = volunteer.name
+  @volunteer = Volunteer.all
+  volunteer.save
+  erb(:volunteer_info)
 end
